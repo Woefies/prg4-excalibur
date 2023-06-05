038be05111d55dce2480fc59dd21bdaf7e9b8c3a
Sem van Elteren <1062594@hr.nl>
2023-06-05 20:53:28 +0200

    edited game over screen

--- src/js/gameover.js
+++ src/js/gameover.js
@@ -1,20 +1,56 @@
+import {Actor, Color, Engine, Font, FontUnit, Label, Physics, Random, Scene, Timer, Vector} from "excalibur"
+import { Resources, ResourceLoader } from './resources.js'
+import {Player} from "./player.js";
 import {Background} from "./background.js";
-import {ResourceLoader} from "./resources.js";
-import {Scene} from "excalibur";
+import {Ground} from "./ground.js";
+import {Enemy} from "./enemy.js";
 
-export class Gameover extends Scene {
-    constructor() {
-        super({
-            width: 1440,
-            height: 700
-        })
-        this.start(ResourceLoader).then(() => this.startGame())
+
+
+
+export class Level1 extends Scene {
+    constructor(engine) {
+        super();
     }
 
-    startGame() {
-        this.showDebug(true)
+
+    game
+    engine
+    score = 0
+
+
+    onInitialize(_engine) {
+        this.game = _engine
+        console.log("gameover")
+
+    }
+
+    onActivate(ctx) {
+
+
         const background = new Background();
         this.add(background)
+
+        this.scorelabel = new Label({
+            text: `Score: ${this.score}`,
+            pos: new Vector(550, 550),
+            font: new Font({
+                family: 'impact',
+                size: 60,
+                unit: FontUnit.Px,
+                color:Color.White
+            })
+        })
+        this.add(this.scorelabel)
+
     }
+
+
+
+
 }
+
+
+
+
 
